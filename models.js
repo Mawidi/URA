a43ba51f13c53bd9c80e030394ca0070eb66bc40
Mawidi <91553452+Mawidi@users.noreply.github.com>
2023-11-27 18:30:00 +0000

    Final
    final examen

--- models.js
+++ models.js
@@ -1,1 +1,39 @@
 //For models
+// models.js
+
+// Function to handle asset transfer
+function transferAsset() {
+    // Get values from input fields
+    var assetCode = document.getElementById('asset_code').value;
+    var newOwnerTIN = document.getElementById('tin').value;
+
+    // Validate input values
+    if (assetCode.trim() === '' || newOwnerTIN.trim() === '') {
+        alert('Please fill in all the required fields.');
+        return;
+    }
+
+    // Assuming you have an API endpoint to update the database with the new owner
+    // You can replace the following code with an actual API call using fetch or any other method
+    // For demonstration purposes, we'll just display a confirmation alert
+    var currentOwner = "Current Owner"; // Replace with actual current owner data
+    var assetName = "Asset Name"; // Replace with actual asset name data
+
+    var confirmationMessage = `Asset: ${assetName}\nCurrent Owner: ${currentOwner}\nNew Owner: ${newOwnerTIN}`;
+    
+    // Display confirmation alert
+    if (confirm(confirmationMessage)) {
+        // Assuming you have a function to update the database (replace with actual implementation)
+        updateDatabase(assetCode, newOwnerTIN);
+
+        // Redirect back to the home page
+        window.location.href = 'index.html';
+    }
+}
+
+// Function to update the database (replace with actual implementation)
+function updateDatabase(assetCode, newOwnerTIN) {
+    // Implement the logic to update the database with the new owner information
+    // This can involve making an API call, sending a request to a server, etc.
+    console.log(`Updating database for Asset ${assetCode} with new owner TIN ${newOwnerTIN}`);
+}
--- server.js
+++ server.js
@@ -1,1 +1,60 @@
-//Express server here.
+const express = require('express');
+const mongoose = require('mongoose'); // Add mongoose import
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const port = 3000;
+
+// MongoDB connection URL and database name
+const url = 'mongodb+srv://nathan:NA3108tha..@cluster0.21pluz0.mongodb.net/URA?retryWrites=true&w=majority'; // Add your MongoDB connection URL
+const dbName = 'URA'; // Add your MongoDB database name 
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/', (req, res) => {
+  const asset = {
+    assetName: req.body.assetName,
+    estimatedCost: parseFloat(req.body.estimatedCost),
+    tin: req.body.tin,
+    type: req.body.type,
+    assetNumber: req.body.assetNumber,
+  };
+
+  // Connect to MongoDB and insert the asset
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      console.error('Error connecting to MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const db = client.db(dbName);
+    const assetsCollection = db.collection('assets');
+
+    assetsCollection.insertOne(asset, (err, result) => {
+      if (err) {
+        console.error('Error inserting asset into MongoDB:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      console.log('Asset inserted:', result.ops[0]);
+      res.send('Asset registered successfully!');
+      client.close();
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
